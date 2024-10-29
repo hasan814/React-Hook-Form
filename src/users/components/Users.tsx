240a11d36712ffe97720d33d23a41315bf2daae0
hasan814 <h.mousavi910@gmail.com>
2024-10-29 09:11:52 +0330

    Old Form Input

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,5 +1,19 @@
+import { useState } from "react";
+
 const Users = () => {
-  return <div>Users</div>;
+  // ========== State ============
+  const [input, setInput] = useState("");
+
+  // ========== Rendering ============
+  return (
+    <input
+      value={input}
+      onChange={(event) => setInput(event.target.value)}
+      name="userName"
+      disabled
+      required
+    />
+  );
 };
 
 export default Users;
